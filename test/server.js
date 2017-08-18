7a660d5de54144a7bd6eb6f5be8b0f3f067ab771
Dan Brown <dbrown.email@gmail.com>
2017-08-18 19:32:28 +0100

    Improve tests

--- test/server.js
+++ test/server.js
@@ -17,8 +17,8 @@ describe('JWT Authentication Service', () => {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-                res.body.should.have.property('success');
-                res.body.should.have.property('message');
+                res.body.should.have.property('success', true);
+                res.body.should.have.property('message', 'User authenticated');
                 res.body.should.have.property('token');
                 done();
             });
@@ -27,11 +27,13 @@ describe('JWT Authentication Service', () => {
 
     // Test the user route
     describe('/GET user', () => {
-        it('it should get a user', (done) => {
+        it('it should fail to get a user', (done) => {
             chai.request(server)
             .get('/user')
             .end((err, res) => {
                 res.should.have.status(400);
+                res.body.should.have.property('success', false);
+                res.body.should.have.property('message', 'Missing authorization token');
                 done();
             });
         });
