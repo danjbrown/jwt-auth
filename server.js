c7291a41714fa06f2d06bd6149d6e21d0dedea76
Dan Brown <dbrown.email@gmail.com>
2017-08-18 16:53:18 +0100

    Fix indent

--- server.js
+++ server.js
@@ -16,53 +16,53 @@ app.use(expressValidator());
 
 // Authenticate the user and return the JWT token
 app.post('/authenticate', function(req, res) {
-	// validate input
-	req.checkBody('username', 'Invalid username').isAlphanumeric();
-	req.checkBody('password', 'Invalid password').isAlphanumeric();
+    // validate input
+    req.checkBody('username', 'Invalid username').isAlphanumeric();
+    req.checkBody('password', 'Invalid password').isAlphanumeric();
 
-	var errors = req.validationErrors();
-	if (errors) {
-		res.status(400).json({success: false, message: errors});
-	}
+    var errors = req.validationErrors();
+    if (errors) {
+        res.status(400).json({success: false, message: errors});
+    }
 
-	// validate the user credentials here
-	var isValid = true;
-	var user = req.body.username;
+    // validate the user credentials here
+    var isValid = true;
+    var user = req.body.username;
 
-	if (isValid) {
-		var tokenData = {
-			username: user
-		}
+    if (isValid) {
+        var tokenData = {
+            username: user
+        }
 
-		var jwtToken = jwt.sign(tokenData, jwtSecretKey, {
-			expiresIn: 3600
-		});
+        var jwtToken = jwt.sign(tokenData, jwtSecretKey, {
+            expiresIn: 3600
+        });
 
-		res.status(200).json({
-			success: true,
-			message: 'User authenticated',
-			token: jwtToken
-		});
-	}
+        res.status(200).json({
+            success: true,
+            message: 'User authenticated',
+            token: jwtToken
+        });
+    }
 });
 
 // Check if the user is authenticated and return the decoded user data
 app.get('/user', function(req, res) {
-	// check header for authentication token
-	var jwtToken = (req.headers.authorization || '').split(' ')[1] || '';
+    // check header for authentication token
+    var jwtToken = (req.headers.authorization || '').split(' ')[1] || '';
 
-	if (jwtToken) {
-		jwt.verify(jwtToken, jwtSecretKey, function(err, decoded) {			
-			if (err) {	
-				res.status(400).json({success: false, message: 'Authentication failed'});	
-			} else {
-				// return the decoded user data
-				res.status(200).json({success: true, message: decoded});
-			}
-		});
-	} else {
-		res.status(400).json({success: false, message: 'Missing authorization token'});		
-	}
+    if (jwtToken) {
+        jwt.verify(jwtToken, jwtSecretKey, function(err, decoded) {			
+            if (err) {	
+                res.status(400).json({success: false, message: 'Authentication failed'});	
+            } else {
+                // return the decoded user data
+                res.status(200).json({success: true, message: decoded});
+            }
+        });
+    } else {
+        res.status(400).json({success: false, message: 'Missing authorization token'});		
+    }
 });
 
 app.listen(port);
